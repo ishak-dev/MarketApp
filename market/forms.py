ab16ced2b2c37154f6d2be99522825017768b00f
ishak-dev <ishak.kazic@gmail.com>
2022-11-22 22:04:35 +0100

    Register validation

--- market/forms.py
+++ market/forms.py
@@ -1,13 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo,Email,DataRequired
+from wtforms.validators import Length, EqualTo,Email,DataRequired,ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
-     username = StringField(label='username',validators=[Length(min=2,max=30),DataRequired()])
-     email_address=StringField(label='email',validators=[Email(),DataRequired()])
-     password1 = PasswordField(label='password',validators=[Length(min=6),DataRequired()])
-     password2 = PasswordField(label='Confirm Password',validators=[EqualTo('password1'),DataRequired()])
-     submit = SubmitField(label='submit')
+
+    def validate_username(self,username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username exist')
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email already used')
+    username = StringField(label='username',validators=[Length(min=2,max=30),DataRequired()])
+    email_address=StringField(label='email',validators=[Email(),DataRequired()])
+    password1 = PasswordField(label='password',validators=[Length(min=6),DataRequired()])
+    password2 = PasswordField(label='Confirm Password',validators=[EqualTo('password1'),DataRequired()])
+    submit = SubmitField(label='submit')
 
 
 
