6d32ca537e1290676c02380be7ba0fd1ab864563
ishak-dev <ishak.kazic@gmail.com>
2022-11-22 21:05:55 +0100

    connect forms with db

--- market/forms.py
+++ market/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo,Email,DataRequired
 
 class RegisterForm(FlaskForm):
-     username = StringField(label='username')
-     email_address=StringField(label='email')
-     password1 = PasswordField(label='password')
-     password2 = PasswordField(label='Confirm Password')
+     username = StringField(label='username',validators=[Length(min=2,max=30),DataRequired()])
+     email_address=StringField(label='email',validators=[Email(),DataRequired()])
+     password1 = PasswordField(label='password',validators=[Length(min=6),DataRequired()])
+     password2 = PasswordField(label='Confirm Password',validators=[EqualTo('password1'),DataRequired()])
      submit = SubmitField(label='submit')
 
 
